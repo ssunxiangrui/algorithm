144eee49732f054d37b684a96200232c3d708410
ssunxiangrui <150216260+ssunxiangrui@users.noreply.github.com>
2023-11-11 12:27:21 +0800

    Create oula_init.cpp

---
+++ oula_init.cpp
@@ -0,0 +1,11 @@
+void oula_init() {
+    flag[1] = 1;
+    for (int i = 1; i < N; i++) {
+        if (flag[i] == 0) p[++tot] = i;
+        for (int j = 1; j <= tot; j++) {
+            if (i * p[j] >= N) break;
+            flag[i * p[j]] = 1;
+            if (i % p[j] == 0) break;
+        }
+    }
+}
