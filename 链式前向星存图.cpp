c084f98517776f2dacddc4e935db8fd68598a468
7_DK <150216260+ssunxiangrui@users.noreply.github.com>
2024-04-09 21:41:30 +0800

    Create 链式前向星存图.cpp

---
+++ 链式前向星存图.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 300010;
+const int M = 600010;
+
+int head[N], nxt[M], to[M], w[M];
+int E, n;
+
+void init() {
+  E = 0;
+  memset(head, -1, sizeof(head));
+}
+void add(int a, int b, int c) {
+  to[E]= b;
+  w[E] = c;
+  nxt[E] = head[a];
+  head[a] = E++;
+}
+int find(int u) {
+  for (int i = head[u]; i != -1; i = nxt[i]) {
+    ......
+  }
+}
+int main() {
+  init();
+  int n;
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    add(a, b, c);
+    add(b, a, c);
+  }
+  return 0;
+}
