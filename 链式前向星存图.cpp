31aaeafe03cf00a068e8b1b9f4bb78319f0d2c0c
7_DK <150216260+ssunxiangrui@users.noreply.github.com>
2024-04-10 21:51:05 +0800

    Update 链式前向星存图.cpp

--- 链式前向星存图.cpp
+++ 链式前向星存图.cpp
@@ -16,16 +16,26 @@ void add(int a, int b, int c) {
 }
 
 //遍历
-int dfs(int u) {
+int dfs(int u, int f) {
   for (int i = head[u]; i != -1; i = nxt[i]) {
-    ......
+    int v = to[u];
+    if (v != f) {
+      dfs(v, u);
+      /*
+       * 其他操作
+       * ......
+       */
+    }
   }
 }
 int main() {
   /*
-   * 初始化
+   * 使用初始化函数
+   * ......
    * 输入和使用建边函数
+   * ......
    * 遍历
+   * ......
    */
   return 0;
 }
