72aa46aaaba75b77a445c63c12796112ac83d901
7_DK <150216260+ssunxiangrui@users.noreply.github.com>
2024-04-10 21:47:53 +0800

    Update 链式前向星存图.cpp

--- 链式前向星存图.cpp
+++ 链式前向星存图.cpp
@@ -1,36 +1,31 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-const int N = 100010;
-const int M = 200010;
-
 int head[N], nxt[M], to[M], w[M];
 int E;
 
+//初始化
 void init() {
   E = 0;
   memset(head, -1, sizeof(head));
 }
+
+//建边
 void add(int a, int b, int c) {
   to[E]= b;
   w[E] = c;
   nxt[E] = head[a];
   head[a] = E++;
 }
+
+//遍历
 int find(int u) {
   for (int i = head[u]; i != -1; i = nxt[i]) {
     ......
   }
 }
 int main() {
-  init();
-  int n;
-  scanf("%d", &n);
-  for (int i = 1; i < n; i++) {
-    int a, b, c;
-    scanf("%d%d%d", &a, &b, &c);
-    add(a, b, c);
-    add(b, a, c);
-  }
+  /*
+   * 初始化
+   * 输入和使用建边函数
+   * 遍历
+   */
   return 0;
 }
